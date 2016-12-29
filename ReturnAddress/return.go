0b9a95211b03fe2ae10cccc50b5e2558bbb31164
Gregor <ghohpe@gmail.com>
2016-12-29 12:09:49 +0100

    added comments

--- ReturnAddress/return.go
+++ ReturnAddress/return.go
@@ -1,3 +1,6 @@
+// Example implementations for Enterprise Integration Patterns
+// www.EnterpriseIntegrationPatterns.com
+//
 // Simple example of ReturnAddress in Go
 
 package ReturnAddress
@@ -25,8 +28,10 @@ func handle(queue chan *Request) {
 func main() {
   reqChannel := make(chan *Request, 10)
   go handle(reqChannel)
+  // Make two requests with separate return channels
   request1 := &Request{[]int{3, 4, 5}, make(chan int)}
   request2 := &Request{[]int{1, 2, 3}, make(chan int)}
+  // Receive both results
   reqChannel <- request1
   reqChannel <- request2
   fmt.Printf("answer: %d %d\n", <-request1.resultChan, <-request2.resultChan)

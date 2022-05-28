090d184f8da30d6fe5bfd184740a10ce6ae898b3
ghohpe <gregor@hohpe.com>
2022-05-28 14:19:07 +0900

    add CreditBureau to PubSub

--- LoanBroker/AwsStepFunctions/PubSub/CreditBureau.js
+++ LoanBroker/AwsStepFunctions/PubSub/CreditBureau.js
@@ -2,20 +2,21 @@ function getRandomInt(min, max) {
   return min + Math.floor(Math.random() * (max-min));
 }
 
-// Expects an event containing the SSN and a RequestId
+// Expects a SSN to return a random credit score. Passes back RequestId.
 exports.handler = async (event) => {
+    // TODO implement
     const min_score = 300
     const max_score = 900
-    
+
     var ssn_regex = new RegExp("^\\d{3}-\\d{2}-\\d{4}$");
     if (ssn_regex.test(event.SSN)) {
         return {
             statusCode: 200,
             request_id: event.RequestId,
             body: {
-                SSN: event.SSN,
-                score: getRandomInt(min_score, max_score),
-                history: getRandomInt(1,30)
+              SSN: event.SSN,
+              score: getRandomInt(min_score, max_score),
+              history: getRandomInt(1,30)
             }
         };
     } else {
@@ -25,6 +26,6 @@ exports.handler = async (event) => {
            body: {
              SSN: event.SSN
            }
-        };            
+        };
     }
 };

4a11b0c31d84a988b5ed82c4b5d5da54bbd858b1
Gregor <ghohpe@gmail.com>
2022-06-07 12:53:15 +0000

    bank sends quote message

--- LoanBroker/GcpWorkflows/Bank/function.js
+++ LoanBroker/GcpWorkflows/Bank/function.js
@@ -1,26 +1,39 @@
 
+const {PubSub} = require('@google-cloud/pubsub');
+const pubSub = new PubSub();
+
 function calcRate(amount, term, score, history) {
   if (amount <= process.env.MAX_LOAN_AMOUNT && score >= process.env.MIN_CREDIT_SCORE) {
    return parseFloat(process.env.BASE_RATE) + Math.random() * ((1000 - score) / 100.0);
   }
 }
 
-exports.bank = (message, context) => {
+exports.bank = async (message, context) => {
     const request = message.data ? Buffer.from(message.data, 'base64').toString() : '{}';
     console.log(request);
     console.log(message.attributes);
-    const data = JSON.parse(request); 
+    const quoteRequest = JSON.parse(request); 
     const requestId = message.attributes["RequestId"];
     const bankId = process.env.BANK_ID;
-	    
-    console.log('Loan Request over %d at credit score %d', data.Amount, data.Credit.Score);
-    const rate = calcRate(data.Amount, data.Term, data.Credit.Score, data.Credit.History);
+    const responseTopic = process.env.QUOTE_RESP_TOPIC;
+
+    response = bankQuote(quoteRequest, bankId);
+    
+    const dataBuffer = Buffer.from(JSON.stringify(response));
+    const customAttributes = { id: requestId };  
+    const messageId = await pubSub.topic(responseTopic).publish(dataBuffer, customAttributes);
+    console.log(`Message ${messageId} published.`);
+}
+
+bankQuote = (quoteRequest, bankId) => {
+    console.log('Loan Request over %d at credit score %d', quoteRequest.Amount, quoteRequest.Credit.Score);
+    const rate = calcRate(quoteRequest.Amount, quoteRequest.Term, quoteRequest.Credit.Score, quoteRequest.Credit.History);
 
     if (rate) {
-      const quote = { "rate": rate, "bankId": bankId, "id": requestId};
-            console.log('Offering Loan', quote);
-            return quote;
+        console.log('%s offering Loan at %f', bankId, rate);
+        return { "rate": rate, "bankId": bankId };
     } else {
-        console.log('Rejecting Loan');
+        console.log('%s rejecting Loan', bankId);
+        return {};
     }
-};
+}

5f0528ddbe67b5c50aa9b6ce53e95c96a9a2b12b
Gregor <ghohpe@gmail.com>
2022-06-08 07:33:21 +0000

    Improved request logging

--- LoanBroker/GcpWorkflows/Bank/function.js
+++ LoanBroker/GcpWorkflows/Bank/function.js
@@ -10,10 +10,10 @@ function calcRate(amount, term, score, history) {
 
 exports.bank = async (message, context) => {
     const request = message.data ? Buffer.from(message.data, 'base64').toString() : '{}';
-    console.log(request);
     console.log(message.attributes);
-    const quoteRequest = JSON.parse(request); 
     const requestId = message.attributes["RequestId"];
+    console.log(request);
+    const quoteRequest = JSON.parse(request); 
     const bankId = process.env.BANK_ID;
     const responseTopic = process.env.QUOTE_RESP_TOPIC;
 

bffdbff0c7fd7383b5e363b2b1a36f8d25b66b09
Gregor <ghohpe@gmail.com>
2022-06-08 07:33:04 +0000

    Added result logging

--- LoanBroker/GcpWorkflows/CreditBureau/function.js
+++ LoanBroker/GcpWorkflows/CreditBureau/function.js
@@ -11,14 +11,16 @@ exports.creditBureau = (req, res) => {
 
   var ssn_regex = new RegExp("^\\d{3}-\\d{2}-\\d{4}$");
   if (ssn_regex.test(req.query.SSN)) {
+    const result = {
+        SSN: req.query.SSN,
+        score: getRandomInt(min_score, max_score),
+        history: getRandomInt(1,30)
+    }
+    console.log("Credit Score:", result)
     res.status(200).send(JSON.stringify(
       {
         request_id: req.query.RequestId,
-        body: {
-          SSN: req.query.SSN,
-          score: getRandomInt(min_score, max_score),
-          history: getRandomInt(1,30)
-        }
+        body: result
       }
     ));
   } else {

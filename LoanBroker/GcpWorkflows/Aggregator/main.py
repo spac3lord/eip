b59cdab04d3311b025bb917ab4d65bbaac9c43f8
Gregor <ghohpe@gmail.com>
2022-06-12 10:27:14 +0000

    Callback implementation

--- LoanBroker/GcpWorkflows/Aggregator/main.py
+++ LoanBroker/GcpWorkflows/Aggregator/main.py
@@ -1,10 +1,15 @@
 from google.cloud import datastore
 import google.cloud.logging
+import google.auth
+import google.auth.transport.requests
 
+creds, project = google.auth.default()
 db = datastore.Client()
 log_client= google.cloud.logging.Client()
 log_client.setup_logging()
+
 import logging
+import requests
 import base64
 import json
 
@@ -23,6 +28,14 @@ def store_quote(correlationId, quote):
     logging.info(item)
     return item
 
+def completion_callback(correlationId, aggregate):
+    logging.info("Aggregate complete: %s", correlationId)
+    url = correlationId
+
+    auth_req = google.auth.transport.requests.Request()
+    creds.refresh(auth_req)
+    status = requests.post(url, json = aggregate, headers = { "Authorization": f"Bearer {creds.token}" } )
+    logging.info("Sent Callback: %i", status.status_code)
 
 def handle_quote(event, context):
     if not 'data' in event:
@@ -33,7 +46,13 @@ def handle_quote(event, context):
     requestId = event['attributes']['id']
     logging.info("%s %s", quote, requestId)
 
+    # Message Filter
+    if not 'bankId' in quote:
+        logging.warning("incomplete quote received")
+        return 0
+
+    # Aggregator
     item = store_quote(requestId, quote)
     if is_complete(item):
-        logging.info("Aggregate %s complete", requestId)
+      completion_callback(requestId, item)  
     return 0
